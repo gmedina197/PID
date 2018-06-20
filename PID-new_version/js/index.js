9d60c1a0b3217e87f7713ad80e396cc4fb023b87
Gabriel Medina Marques <gmedina197@gmail.com>
2018-06-20 20:10:06 -0300

    piika

---
+++ PID-new_version/js/index.js
@@ -0,0 +1,94 @@
+let referenceImgDom = document.getElementById('picField-1'),
+    canvasRI = document.getElementById('canvas-1'),
+    adjustImgDom = document.getElementById('picField-2'),
+    canvasAI = document.getElementById('canvas-2');
+
+let refImage,
+    adjImage,
+    grayScaleMatrix = [];
+
+function uploadRef() {
+    refImage = new SimpleImage(referenceImgDom);
+    refImage.drawTo(canvasRI);
+    init();
+}
+
+function uploadAdj() {
+    adjImage = new SimpleImage(adjustImgDom);
+    adjImage.drawTo(canvasAI);
+}
+
+function makeGray() {
+    let grayScaleList = [];
+    if (refImage) {
+        for (let pixel of refImage.pixels()) {
+            let avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
+            grayScaleList.push(avg);
+            pixel.setRed(avg);
+            pixel.setGreen(avg);
+            pixel.setBlue(avg);
+        }
+        grayScaleMatrix = listToMatrix(grayScaleList, refImage.getHeight());
+        refImage.drawTo(canvasRI);
+    }
+
+    if (adjImage) {
+        for (let pixel of adjImage.pixels()) {
+            let avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
+            pixel.setRed(avg);
+            pixel.setGreen(avg);
+            pixel.setBlue(avg);
+        }
+        //let pixel = image.getPixel(0, 0);
+        adjImage.drawTo(canvasAI);
+    }
+}
+
+function listToMatrix(list, elementsPerSubArray) {
+    var matrix = [],
+        i,
+        k;
+
+    for (i = 0, k = -1; i < list.length; i++) {
+        if (i % elementsPerSubArray === 0) {
+            k++;
+            matrix[k] = [];
+        }
+
+        matrix[k].push(list[i]);
+    }
+    return matrix;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+        direction: 'right',
+        hoverEnabled: false
+    });
+});
+
+function uploadRefImage() {
+    document.getElementById('picField-1').click();
+}
+
+function uploadAdjImage() {
+    document.getElementById('picField-2').click();
+}
+
+//hide the obj to style the float button
+referenceImgDom.style.display = 'none';
+adjustImgDom.style.display = 'none';
+
+referenceImgDom.addEventListener('change', uploadRef);
+adjustImgDom.addEventListener('change', uploadAdj);
+
+function resetRefImage() {
+    let ctx = canvasRI.getContext('2d');
+    ctx.clearRect(0, 0, canvasRI.width, canvasRI.height);
+}
+
+function resetAdjImage() {
+    let ctx = canvasAI.getContext('2d');
+    ctx.clearRect(0, 0, canvasAI.width, canvasAI.height);
+}
---
+++ PID-new_version/readme.txt
@@ -0,0 +1,3 @@
+Ativar js experimental no Chrome
+Na barra de endereço digite 'chrome://flags/#enable-javascript-harmony'
+e ative o javascript experimental
