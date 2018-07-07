122312288ac4784d58a998d470aa46116806b995
Gabriel Medina <gmedina197@gmail.com>
2018-07-07 01:51:34 -0300

    Some bugs fixed

--- js/drawnRec.js
+++ js/drawnRec.js
@@ -1,5 +1,5 @@
-let canvas = document.getElementById('canvas-1'),
-    ctx = canvas.getContext('2d'),
+let canvas = document.getElementById("canvas-1"),
+    ctx = canvas.getContext("2d"),
     rect = {
         startX: 0,
         startY: 0,
@@ -8,6 +8,10 @@ let canvas = document.getElementById('canvas-1'),
     },
     fullImage = false,
     medias = {
+        ref: 0,
+        adj: 0
+    },
+    variances = {
         ref: 0,
         adj: 0
     },
@@ -26,9 +30,9 @@ function getMousePos(canvas, evt) {
 }
 
 function init() {
-    canvas.addEventListener('mousedown', mouseDown, false);
-    canvas.addEventListener('mouseup', mouseUp, false);
-    canvas.addEventListener('mousemove', mouseMove, false);
+    canvas.addEventListener("mousedown", mouseDown, false);
+    canvas.addEventListener("mouseup", mouseUp, false);
+    canvas.addEventListener("mousemove", mouseMove, false);
 }
 
 function mouseDown(evt) {
@@ -56,7 +60,7 @@ function mouseMove(evt) {
 
 function draw() {
     ctx.lineWidth = 1;
-    ctx.strokeStyle = 'yellow';
+    ctx.strokeStyle = "yellow";
     ctx.strokeRect(rect.startX, rect.startY, rect.w, rect.h);
 }
 
@@ -64,7 +68,7 @@ function makeFullImgMatrix() {}
 
 function makeSelectedMatrix(area) {
     //fullimg use
-    ctx.strokeStyle = 'green';
+    ctx.strokeStyle = "green";
     ctx.strokeRect(rect.startX, rect.startY, rect.w, rect.h);
 
     let adjacencyList = new Array(area.h).fill(0); // w * h
@@ -79,10 +83,12 @@ function makeSelectedMatrix(area) {
     } else {
         makeFullImgMatrix();
     }
-    medias.ref = getMedia(refImageMatrix)
-    medias.adj = getMedia(adjImageMatrix)
-    console.log(medias);
-    
+    medias.ref = getMedia(refImageMatrix);
+    medias.adj = getMedia(adjImageMatrix);
+
+    variances.ref = getVariation(refImageMatrix, medias.ref);
+    variances.adj = getVariation(adjImageMatrix, medias.adj);
+    console.log(variances);
 }
 
 //return the average on a list of pixels
@@ -93,9 +99,21 @@ function getMedia(list) {
             element += list[i][j];
         }
     }
-    console.log(element);
-    
 
-    return element / list.length;
+    return element / (list.length * list[0].length);
 }
 
+function getVariation(list, media) {
+    let element = 0;
+    for (let i = 0; i < list.length; i++) {
+        for (let j = 0; j < list[0].length; j++) {
+            element += Math.pow(list[i][j] - media, 2);
+        }
+    }
+
+    return element / (list.length * list[0].length);
+}
+
+function run() {
+    
+}
